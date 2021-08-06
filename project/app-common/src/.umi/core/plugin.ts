7fdc672daccaa5be89d9949a702af502be36a2ab
vannvan <862670198@qq.com>
2021-08-06 11:52:12 +0800

    init

---
+++ project/app-common/src/.umi/core/plugin.ts
@@ -0,0 +1,8 @@
+// @ts-nocheck
+import { Plugin } from '/Users/vannvan/Documents/vannvan@github/umi-qiankun-explore/node_modules/@umijs/runtime';
+
+const plugin = new Plugin({
+  validKeys: ['modifyClientRenderOpts','patchRoutes','rootContainer','render','onRouteChange','__mfsu',],
+});
+
+export { plugin };
